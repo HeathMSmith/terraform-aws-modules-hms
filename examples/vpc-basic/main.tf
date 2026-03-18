33f25309da297b95b90b67ed93ad0def54c03f29
Heath Smith <heathsmithaws2025@gmail.com>
2026-03-18 11:52:24 -0400

    Final tweaks and bug fixes on VPC module

--- examples/vpc-basic/main.tf
+++ examples/vpc-basic/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "vpc" {
   source = "../../modules/vpc"
 
-  vpc_cidr = "10.0.0.10/16"
+  vpc_cidr = "10.0.0.0/16"
 
   public_subnet_cidrs = [
     "10.0.1.0/24",
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -3,7 +3,7 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-  value = aws_subnet.public.id
+  value = aws_subnet.public[*].id
 }
 
 output "internet_gateway_id" {
